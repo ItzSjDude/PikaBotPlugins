adcc0f037105e3a8196fc5769d577d0e18af98d1
Sachin Jangir <support@ItzSjDude.in>
2021-03-19 20:41:06 +0530

    Update Var.py

--- plugins/Var.py
+++ plugins/Var.py
@@ -1,4 +1,4 @@
- class Var(object):
+class Var(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
     STR1 = os.environ.get("STRING_SESSION", None)
