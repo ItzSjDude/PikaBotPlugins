7df439128841396bbdb7584f3b7c57e37f2dd797
Sachin Jangir <support@ItzSjDude.in>
2021-04-09 00:01:23 +0530

    Update acc_profile.py

--- plugins/acc_profile.py
+++ plugins/acc_profile.py
@@ -22,35 +22,42 @@ from . import anpfp, atb, atnm, avpfp, gmpfp, pbio, pname
 
 
 @ItzSjDude(outgoing=True, pattern="pbio (.*)")
+@ItzSjDude(sudo=True, pattern="pbio (.*)")
 async def _(event):
     await pbio(event)
 
 
 @ItzSjDude(outgoing=True, pattern="pname ((.|\n)*)")
+@ItzSjDude(sudo=True, pattern="pname ((.|\n)*)")
 async def _(event):
     await pname(event)
 
 
 @ItzSjDude(outgoing=True, pattern="animepfp ?(.*)")
+@ItzSjDude(sudo=True, pattern="animepfp ?(.*)")
 async def _(event):
     await anpfp(event)
 
 
 @ItzSjDude(outgoing=True, pattern="avengerspfp ?(.*)")
+@ItzSjDude(sudo=True, pattern="avengerspfp ?(.*)")
 async def _(event):
     await avpfp(event)
 
 
 @ItzSjDude(outgoing=True, pattern="gamerpfp ?(.*)")
+@ItzSjDude(sudo=True, pattern="gamerpfp ?(.*)")
 async def _(event):
     await gmpfp(event)
 
 
 @ItzSjDude(outgoing=True, pattern="autoname$")
+@ItzSjDude(sudo=True, pattern="autoname$")
 async def _(event):
     await atnm(event)
 
 
 @ItzSjDude(outgoing=True, pattern="autobio$")
+@ItzSjDude(sudo=True, pattern="autobio$")
 async def _(event):
     await atb(event)
