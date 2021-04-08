2161e3ea4cf51fd461072902fd66ae16966239fe
ItzSjDude <Support@itzsjdude.in>
2021-04-08 10:03:26 +0000

    PikaBot Fixes🚶

--- plugins/_helper.py
+++ plugins/_helper.py
@@ -1,5 +1,8 @@
-from . import bot, bot2, bot3, bot4, helper
+from . import helper
+
 # ____Main/Multiclients____
+
+
 @ItzSjDude(outgoing=True, pattern=r"help ?(.*)")
 async def _(event):
     await helper(event)
