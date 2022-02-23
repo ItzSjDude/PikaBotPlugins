3a6b44ba6dfb1f5d727a06eb9c04ef5ea1d588b5
Sachin Jangir <support@ItzSjDude.in>
2022-02-23 07:45:58 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3509,47 +3509,48 @@ async def _eval(event):
     redirected_error = sys.stderr = io.StringIO()
     stdout, stderr, exc = None, None, None
 
-    try:
-        await aexec(cmd, event)
-    except Exception:
-        exc = traceback.format_exc()
+    if cmd: 
+        try:
+            await aexec(cmd, event)
+        except Exception:
+            exc = traceback.format_exc()
 
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
+        stdout = redirected_output.getvalue()
+        stderr = redirected_error.getvalue()
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
 
-    evaluation = ""
-    if exc:
-        evaluation = exc.replace(evlrp, "")
-    elif stderr:
-        evaluation = stderr.replace(evlrp, "")
-    elif stdout:
-        evaluation = stdout
-    else:
-        evaluation = "Success"
+        evaluation = ""
+        if exc:
+            evaluation = exc.replace(evlrp, "")
+        elif stderr:
+            evaluation = stderr.replace(evlrp, "")
+        elif stdout:
+            evaluation = stdout
+        else:
+            evaluation = "Success"
       
-    final_output = "**Python Code**: \n`{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
+        final_output = "**Python Code**: \n`{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
 
-    if len(final_output) > 4096:
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id,
-            )
-            await event.delete()
-    else:
-        await infx_msg(ax_, final_output)
+        if len(final_output) > 4096:
+            with io.BytesIO(str.encode(final_output)) as out_file:
+                out_file.name = "eval.text"
+                await event.client.send_file(
+                    event.chat_id,
+                    out_file,
+                    force_document=True,
+                    allow_cache=False,
+                    caption=cmd,
+                    reply_to=reply_to_id,
+                )
+                await event.delete()
+        else:
+            await infx_msg(ax_, final_output)
 
 
-async def aexec(code, event):
-    exec(f"async def __aexec(event): " + "".join(f"\n {l}" for l in code.split("\n")))
-    return await locals()["__aexec"](event)
+    async def aexec(code, event):
+        exec(f"async def __aexec(event): " + "".join(f"\n {l}" for l in code.split("\n")))
+        return await locals()["__aexec"](event)
 
 
 async def helper(event):
