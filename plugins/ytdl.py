50bfbe15cdaa6a343029c230768f150a3e614e00
Sachin Jangir <support@ItzSjDude.in>
2023-06-08 09:44:45 +0530

    Update ytdl.py

--- plugins/ytdl.py
+++ plugins/ytdl.py
@@ -12,8 +12,8 @@ import os
 import time
 
 from telethon.tl.types import DocumentAttributeAudio
-from youtube_dl import YoutubeDL
-from youtube_dl.utils import (
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import (
     ContentTooShortError,
     DownloadError,
     ExtractorError,
